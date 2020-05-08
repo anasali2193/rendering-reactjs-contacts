2928cdc19cf4497951c547f7e5bca8019f9b1b18
anasali2193 <anasali2193@gmail.com>
2020-05-08 15:13:23 +0500

    Update map function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React , {Component} from 'react';
+import ListContacts from './ListContacts';
 
-function App() {
-  const people = [
-    {name : 'taylor'},
-    {name : 'sara'},
-    {name : 'richard'},
-    
-  ]
-  return (
-      
-    <ol>
-      {people.map((person)=>(
-        <li key={person.name}>{person.name}</li>
-      )) }
+class App extends Component {
+  render (){
+    const contacts = [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ];
 
-    </ol>
-    
-    );
+    //  const contast = [
+    //    {
+    //      "id" : "karen"
+
+    //  }];
+
+    return(
+      <div>
+      <ListContacts contacts={contacts}/>
+      </div>
+    )
+    }   
 }
 
 export default App;
